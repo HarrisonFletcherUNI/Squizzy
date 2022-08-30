2b6ace740adc27b74ac7e1a2345e7ba9e6f4d00f
Harrison Fletcher <HarrisonFletcherUNI@outlook.com>
2022-08-30 10:29:04 +1000

    quick change

--- SquizzyIFB399/Assets/Scripts/QuizManager.cs
+++ SquizzyIFB399/Assets/Scripts/QuizManager.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
+    public string quizLocationName,quizLocationAddress;
     public QuestionData[] questions;
     private int quizSize;
 
-    public GameObject answer1, answer2, answer3, answer4;
+    public TextMeshProUGUI scoreText, timerText;
+    private float finalScore, finalTime;
+
+    [Header("Answer Buttons")]
+    public GameObject answer1Object;
+    public GameObject answer2Object;
+    public GameObject answer3Object;
+    public GameObject answer4Object;
+
+    bool timerActive;
     
     // Start is called before the first frame update
     void Start()
     {
         // Get quizz length
         quizSize = questions.Length;
+
+        timerActive = true;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (timerActive)
+        {
+            Timer();
+        }
+    }
+
+    void Timer()
     {
         
     }
