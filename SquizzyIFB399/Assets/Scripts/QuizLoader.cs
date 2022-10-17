e5f1dfc8cfc98edf610e9ee821fffc3e32712af4
CamGood01 <campbellgoodchild@gmail.com>
2022-10-17 15:40:14 +1000

    uploader Script Reader
    Uploaded Script reader

---
+++ SquizzyIFB399/Assets/Scripts/QuizLoader.cs
@@ -0,0 +1,49 @@
+﻿
+using System.IO;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace SquizzyReader
+{
+
+    
+
+    
+    class QuizLoader
+    {
+        // Default folder    
+           
+  
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader(@"C:\Users\campb\OneDrive\Documents\Capstone Project Pt.1\test.json"))
+            {
+                string json = r.ReadToEnd();
+                List<QuestionData> questions = JsonConvert.DeserializeObject<List<QuestionData>>(json);
+                foreach (QuestionData question in questions)
+                {
+                    Console.WriteLine("{0} {1}", question.questionText, question.correctAnswer);
+                }
+                
+            }
+        }
+
+        public class QuestionData
+        {
+
+            public string questionText;
+            public string answerA, answerB, answerC, answerD;
+            public string correctAnswer;
+        }
+        static void Main(string[] args)
+        {
+
+            LoadJson();
+           
+
+            Console.ReadKey();
+        }
+    }
+}
